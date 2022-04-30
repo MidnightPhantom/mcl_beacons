e2f9ac1fb8ffeb5020179ef5530e1065275c6b4b
MidnightPhantom <96172170+MidnightPhantom@users.noreply.github.com>
2022-04-30 14:52:57 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,11 @@
 
-minetest.register_node(mcl_beacons:beacon),
+minetest.register_node("mcl_beacons:beacon"),
            description("beacon"),
-minetest.register_craft
+minetest.register_craft({
+                 output = "mcl_beacons:beacon",
+                 recipie ={
+                           {"mcl_core:glass","mcl_core:glass","mcl_core:glass"},
+                           {"mcl_core:glass", "", "mcl_core:glass"},
+                           {"mcl_core:obsidian", "mcl_core:obsidian", "mcl_core:obsidian"},
+                           }
+})      
