bd9c1a5f5ddc8eb9590e44d2afa212e312ef8018
MidnightPhantom <96172170+MidnightPhantom@users.noreply.github.com>
2022-07-18 16:27:22 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,11 @@
 
 minetest.register_node("mcl_beacons:beacon"),
            description = ("beacon"),
+           tiles = {"mcl_beacons_beacon_top.png","mcl_beacons_beacon_bottom.png","mcl_beacons_beacon_side.png"},
+           drawtype = "glasslike",
+           light_source = minetest.LIGHT_MAX,
+           is_ground_content = false,
+           paramtype = "light",
            
 minetest.register_craft({
                  output = "mcl_beacons:beacon",
